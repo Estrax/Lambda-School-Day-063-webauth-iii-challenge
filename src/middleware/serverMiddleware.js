dc31df7f9623985ed601495b6bd8e71e37d73ba7
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-03 16:47:32 -0400

    added auth routes and updated server middleware

--- src/middleware/serverMiddleware.js
+++ src/middleware/serverMiddleware.js
@@ -2,9 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const session = require('express-session');
-const knexConfig = require('../database/dbConfig');
-const knexSessionStore = require('connect-session-knex')(session);
 
 require('dotenv').config();
 
@@ -13,23 +10,5 @@ module.exports = (app) => {
     app.use(express.urlencoded({ extended: false }));
     app.use(morgan('dev'));
     app.use(helmet());
-    app.use(session({
-        name: 'sessionName',
-        secret: process.env.SECRET_KEY || 'secretKey',
-        cookie: {
-            maxAge: 1000*60*15,
-            secure: false,
-            httpOnly: true
-        },
-        resave: false,
-        saveUninitialized: false,
-        store: new knexSessionStore({
-            knex: knexConfig,
-            tablename: 'sessions',
-            sidfieldname: 'sid',
-            createtable: true,
-            clearInterval: 1000*60*60
-        })
-    }));
     app.use(cors());
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+const auth = require('./auth');
+router.use('/', auth);
+
 router.route('/')
     .get(async (req, res) => {
         res.json({
